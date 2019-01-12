3385ad30ae5cf05d35b70112384abe8b4f46096a
Vadym Kotlyarenko <vadymkotyarenko@gmail.com>
2019-01-12 19:30:36 +0200

    frontend-nanodegree-arcade-game

--- js/app.js
+++ js/app.js
@@ -1,39 +1,64 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    this.x += dt * this.speed;    
+    if(Math.abs(this.x - player.x) < 70 && Math.abs(this.y - player.y) < 50) {
+        player.x = 200; 
+        player.y = 400;
+    }
+    if (this.x > 650) {
+        this.x = Math.floor(Math.random(-400, -100));
+        this.speed = this.speed * Math.floor(Math.round(1, 10));
+    }
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+};
 
+Player.prototype.update = function(dt) {
+    if (this.y <= 40  || this.y >= 450 || this.x <= -40  || this.x >= 440) {
+        this.x = 200; 
+        this.y = 400;
+    }
+};
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
+Player.prototype.handleInput = function(allowedKey) {
+    switch (allowedKey){
+        case 'left':
+            this.x -= 30;
+            break;
+        case 'up':
+            this.y -= 30;
+            break;
+        case 'right':
+            this.x += 30;
+            break;
+        case 'down':
+            this.y += 30;
+            break;
+    }
+};
 
+var player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+var allEnemies = [new Enemy(-50, 60, 100), new Enemy(-150, 140, 100), new Enemy(-100, 230, 100)]
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -41,6 +66,6 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
